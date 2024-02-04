666abae782ae0ab1b9f0523bb395debaaeed28de
danimisme <msubhanr53@gmail.com>
2024-02-04 16:56:07 +0700

    membuat tampilan kalkulator

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container-lg col-lg-6 col-md-8 col-12">
+      <h1>My Calculator</h1>
+      <DisplayInput />
+      <Buttons />
+    </div>
+  );
 }
 
-export default App
+const DisplayInput = () => {
+  return (
+    <div className=" fs-1 text-end border border-1 p-2 border-dark ">0</div>
+  );
+};
+
+const Buttons = () => {
+  return (
+    <div className=" justify-content-center gap-2 my-2 p-2  ">
+      <div className=" d-flex gap-2 w-100 mb-2">
+        <button className="btn btn-primary w-100 py-4 fs-3">AC</button>
+        <button className="btn btn-primary w-100 py-4 fs-3">C</button>
+        <button className="btn btn-primary w-100 py-4 fs-3">%</button>
+        <button className="btn btn-primary w-100 py-4 fs-3">/</button>
+      </div>
+      <div className=" d-flex gap-2 w-100 mb-2">
+        <button className="btn btn-primary w-100 py-4 fs-3">7</button>
+        <button className="btn btn-primary w-100 py-4 fs-3">8</button>
+        <button className="btn btn-primary w-100 py-4 fs-3">9</button>
+        <button className="btn btn-primary w-100 py-4 fs-3">*</button>
+      </div>
+      <div className=" d-flex gap-2 w-100 mb-2">
+        <button className="btn btn-primary w-100 py-4 fs-3">4</button>
+        <button className="btn btn-primary w-100 py-4 fs-3">5</button>
+        <button className="btn btn-primary w-100 py-4 fs-3">6</button>
+        <button className="btn btn-primary w-100 py-4 fs-3">-</button>
+      </div>
+      <div className=" d-flex gap-2 w-100 mb-2">
+        <button className="btn btn-primary w-100 py-4 fs-3">1</button>
+        <button className="btn btn-primary w-100 py-4 fs-3">2</button>
+        <button className="btn btn-primary w-100 py-4 fs-3">3</button>
+        <button className="btn btn-primary w-100 py-4 fs-3">+</button>
+      </div>
+      <div className=" d-flex gap-2 w-100 mb-2">
+        <button className="btn btn-primary w-100 py-4 fs-3">0</button>
+        <button className="btn btn-primary w-100 py-4 fs-3">.</button>
+        <button className="btn btn-primary w-100 py-4 fs-3">=</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
